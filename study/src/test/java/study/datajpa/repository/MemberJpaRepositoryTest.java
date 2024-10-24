75c615164157a22ea7a8fc632b0c0bf75ffbdffc
JUNGHOON AN <gnstm831@naver.com>
2024-10-24 22:09:33 +0900

    섹션2. 프로젝트 환경설정
    4. 스프링 데이터 JPA와 DB 설정, 동작확인

---
+++ study/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,35 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberJpaRepositoryTest {
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        // given
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        // when
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
+        assertThat(findMember).isEqualTo(member);
+
+    }
+}
