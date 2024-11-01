c01b26f4fe3829089ecae8a75da2c9d27b37d39e
JUNGHOON AN <gnstm831@naver.com>
2024-11-01 12:32:49 +0900

    섹션5. 쿼리 메서드 기능
    6. 반환 타입

--- study/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ study/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -164,5 +165,22 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member ("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> findListByUsername = memberRepository.findListByUsername("AAA");
+        System.out.println("findListByUsername = " + findListByUsername);
+
+        Member findMemberByUsername = memberRepository.findMemberByUsername("AAA");
+        System.out.println("findMemberByUsername = " + findMemberByUsername);
+
+        Optional<Member> findOptionalByUsername = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("findOptionalByUsername = " + findOptionalByUsername.get());
     }
 }
